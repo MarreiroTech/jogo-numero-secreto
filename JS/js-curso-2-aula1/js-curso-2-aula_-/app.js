eb966656000f2053665f39a70f5c828a067797c6
Clayton Marreiro <marreirotech@gmail.com>
2024-02-06 15:04:33 -0300

    alterado limite de números para 100

--- JS/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ JS/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -5,7 +5,7 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 100';
 
 let nSorteados = [];
-let nLimite = 10;
+let nLimite = 100;
 let nSecreto = gerarNum();
 let nTentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
 exibirTextoNaTela('h1','Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
